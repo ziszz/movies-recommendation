27cf7bc050ca301e2a847db9b60abb348c9216c5
Abdul Azis <abdul.azizz1108@gmail.com>
2022-11-25 11:34:58 +0700

    unfinish model trainer

---
+++ movie_transform.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+FEATURE_KEYS = [
+    "title"
+    "genres"
+]
+
+def transformed_name(key):
+    return f"{key}_xf"
+
+def preprocessing_fn(inputs):
+    outputs = {}
+    
+    for key in FEATURE_KEYS:
+        outputs[transformed_name(key)] = tf.strings.lower(
+            inputs[key]
+        )
+    
+    return outputs

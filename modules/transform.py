e227a42f58309cd1b5a01b1c0c44f3fd0d0b739b
Abdul Azis <abdul.azizz1108@gmail.com>
2022-11-26 22:14:33 +0700

    create transform module

--- modules/transform.py
+++ modules/transform.py
@@ -1,16 +1,29 @@
-import tensorflow as tf
+import os
 
-FEATURE_KEY = "title"
+import tensorflow as tf
+import tensorflow_transform as tft
+from absl import logging
+
+NUMERICAL_FEATURE = "userId"
+CATEGORICAL_FEATURE = "title"
+
 
 def transformed_name(key):
     return f"{key}_xf"
 
 
 def preprocessing_fn(inputs):
-    outputs = {}
+    try:
+        outputs = {}
 
-    for key in FEATURE_KEY:
-        outputs[transformed_name(key)] = tf.strings.lower(
-            inputs[key]
+        outputs[CATEGORICAL_FEATURE] = tft.compute_and_apply_vocabulary(
+            tf.strings.strip(inputs[CATEGORICAL_FEATURE]),
+            num_oov_buckets=1,
+            vocab_filename=os.path.abspath(f"vocabulary/{CATEGORICAL_FEATURE}"),
         )
+
+        outputs[NUMERICAL_FEATURE] = tf.cast(inputs[NUMERICAL_FEATURE], tf.int64)
+    except BaseException as err:
+        logging.error(f"ERROR IN _get_model:\n{err}")
+        
     return outputs

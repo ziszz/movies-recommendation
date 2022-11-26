98c413e7f22ce79571a26559637a02e8ae758e16
Abdul Azis <abdul.azizz1108@gmail.com>
2022-11-26 22:28:30 +0700

    change transform code

--- modules/transform.py
+++ modules/transform.py
@@ -4,8 +4,10 @@ import tensorflow as tf
 import tensorflow_transform as tft
 from absl import logging
 
-NUMERICAL_FEATURE = "userId"
-CATEGORICAL_FEATURE = "title"
+FEATURES = [
+    "userId",
+    "movieId",
+]
 
 
 def transformed_name(key):
@@ -16,14 +18,9 @@ def preprocessing_fn(inputs):
     try:
         outputs = {}
 
-        outputs[CATEGORICAL_FEATURE] = tft.compute_and_apply_vocabulary(
-            tf.strings.strip(inputs[CATEGORICAL_FEATURE]),
-            num_oov_buckets=1,
-            vocab_filename=os.path.abspath(f"vocabulary/{CATEGORICAL_FEATURE}"),
-        )
-
-        outputs[NUMERICAL_FEATURE] = tf.cast(inputs[NUMERICAL_FEATURE], tf.int64)
+        for key in FEATURES:
+            outputs[key] = tf.cast(inputs[key], tf.int64)
     except BaseException as err:
         logging.error(f"ERROR IN _get_model:\n{err}")
-        
+
     return outputs

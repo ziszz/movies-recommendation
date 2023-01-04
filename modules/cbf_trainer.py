1ada3afe0dba83a7d73e41fc4a4c8bcf470c5bed
Abdul Azis <abdul.azizz1108@gmail.com>
2023-01-04 12:42:47 +0700

    change early stop metrics

--- modules/cbf_trainer.py
+++ modules/cbf_trainer.py
@@ -112,8 +112,8 @@ def run_fn(fn_args):
 
         tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir)
 
-        sim_early_stop_callbacks = keras.callbacks.EarlyStopping(
-            monitor="val_cosine_similarity",
+        rmse_early_stop_callbacks = keras.callbacks.EarlyStopping(
+            monitor="val_root_mean_squred_error",
             mode="min",
             verbose=1,
             patience=10,
@@ -128,7 +128,7 @@ def run_fn(fn_args):
 
         callbacks = [
             tensorboard_callback,
-            sim_early_stop_callbacks,
+            rmse_early_stop_callbacks,
             loss_early_stop_callbacks,
         ]
 
